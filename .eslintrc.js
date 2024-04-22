490c8ebcd7fa966ae67596c1885d761e60ca96f8
guilhermeqmaia <guilherme.q.maia.contato@gmail.com>
2024-04-22 19:50:16 -0300

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,11 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    'plugin:@typescript-eslint/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
     'plugin:prettier/recommended',
+    "prettier"
   ],
   root: true,
   env: {
@@ -21,5 +24,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'semi': ['error', 'always']
   },
 };
