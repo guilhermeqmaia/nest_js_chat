0426213a83f784356c2855b65d1daf70e195ae0c
guilhermeqmaia <guilherme.q.maia.contato@gmail.com>
2024-04-24 20:42:29 -0300

    stop returning objects in controller, sending data using response now

--- src/module/user/user_controller.ts
+++ src/module/user/user_controller.ts
@@ -3,9 +3,11 @@ import {
   Get,
   Post,
   Req,
+  Res,
   UnprocessableEntityException,
 } from "@nestjs/common";
-import { User, isValidUser } from "./user_model";
+import { Request, Response } from "express";
+import { isValidUser } from "./user_model";
 import { UserService } from "./user_service";
 
 @Controller("user")
@@ -13,16 +15,18 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post("/signup")
-  signUp(@Req() req: Request): number {
+  signUp(@Req() req: Request, @Res() res: Response) {
     const body = req.body;
     if (!isValidUser(body)) {
       throw new UnprocessableEntityException("User data is not valid");
     }
-    return this.userService.signUp(body);
+    const userId = this.userService.signUp(body);
+    res.status(201).send({ userId: userId });
   }
 
   @Get("/list")
-  list(): User[] {
-    return this.userService.list();
+  list(@Res() res: Response) {
+    const list = this.userService.list();
+    res.status(200).send(list);
   }
 }
